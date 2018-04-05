85037fdb978f9d8ed2a4692d8ed015fcbf0433c8
lindenb <plindenbaum@yahoo.fr>
2018-04-05 17:31:44 +0200

    process doesn't like full paths in shells

--- recipes/jvarkit/2018.04.05/build.sh
+++ recipes/jvarkit/2018.04.05/build.sh
@@ -6,6 +6,10 @@ outdir=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
 mkdir -p "${outdir}/dist"
 mkdir -p "${PREFIX}/bin"
 make vcffilterjdk bioalcidaejdk samjdk vcf2table prettysam standalone=yes dist.dir=${outdir}/dist
+
+#problem with full path detected in shells
+find ${outdir}/dist -type f  \! -name "*.jar" -delete
+
 cp "${RECIPE_DIR}/jvarkit.sh" "${outdir}/dist/jvarkit"
 chmod +x "${outdir}/dist/jvarkit"
 ln -s "${outdir}/dist/jvarkit" "${PREFIX}/bin"
