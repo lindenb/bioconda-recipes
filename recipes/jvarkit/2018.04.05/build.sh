8a172ef81bfb6efaf0f0dc364391e336e532cc63
lindenb <plindenbaum@yahoo.fr>
2018-04-05 16:55:21 +0200

    fixing cp build.sh

--- recipes/jvarkit/2018.04.05/build.sh
+++ recipes/jvarkit/2018.04.05/build.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+set -eu -o pipefail
 
 outdir=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
 
 mkdir -p "${outdir}/dist"
 make vcffilterjdk bioalcidaejdk samjdk vcf2table prettysam standalone=yes dist.dir=${outdir}/dist
-cp jvarkit.sh "${outdir}/dist/jvarkit"
+cp "${RECIPE_DIR}/jvarkit.sh" "${outdir}/dist/jvarkit"
+chmod 0755 "${outdir}/dist/jvarkit"
 
